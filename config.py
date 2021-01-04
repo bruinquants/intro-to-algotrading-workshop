7b9e5016ae6e32a5a52a9b16240b0eb2b281055d
Colin Curtis <colinpcurtis826@ucla.edu>
2021-01-04 11:04:27 -0600

    remove

--- config.py
+++
@@ -1,6 +0,0 @@
-BASE_URL = "https://paper-api.alpaca.markets"
-KEY = "<Your API Key Here>"
-SECRET_KEY = "<Your Secret Key Here>"
-
-HEADERS = {"APCA-API-KEY-ID": KEY, "APCA-API-SECRET-KEY": SECRET_KEY}
-# headers are used to authenticate our request
